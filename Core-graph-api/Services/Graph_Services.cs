1ac023e254f4d9778adf2e08408b946e0d1ed0e2
Max Sihuar Holguino Nuñez <maxsihuar98@gmail.com>
2026-06-28 13:45:15 -0500

    xd

--- Core-graph-api/Services/Graph_Services.cs
+++ Core-graph-api/Services/Graph_Services.cs
@@ -3,5 +3,16 @@ namespace Core_graph_api.Services
 {
     public class Graph_Services
     {
+        private readonly Graph _graph;
+
+        public Graph_Services(Graph graph) 
+        {
+            _graph = graph;
+        }
+
+        public List<Node> BFS_Graph()
+        {
+
+        }
     }
 }
